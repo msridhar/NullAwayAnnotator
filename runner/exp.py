9e44300813468a2116e232fdb9943aa559f2f720
nimakarimipour <karimipour.nima@gmail.com>
2022-03-28 18:50:53 -0700

    update

--- runner/exp.py
+++ runner/exp.py
@@ -2,33 +2,46 @@ import os
 import json
 import time
 
-f = open("config.json", "r")
-data = json.load(f)
+config = json.load(open("config.json", "r"))
+projects = json.load(open("projects.json", "r"))
+root = "/home/nima/Developer/AutoFixer/Evaluation/Projects/{}"
 
-for i in range(0, 1):
-    print("START")
-    branch = "deep_{}".format(i)
-    COMMAND = "cd {} && {}".format(data['PROJECT_PATH'], "{}")
-    os.system(COMMAND.format("git reset --hard"))
-    os.system(COMMAND.format("git checkout nullaway"))
-    os.system(COMMAND.format("git branch -D {}".format(branch)))
-    os.system(COMMAND.format("git push origin --delete {}".format(branch)))
-    os.system(COMMAND.format("git checkout -b {}".format(branch)))
-    data["DEPTH"] = i
+for proj in projects['projects']:
+    if not proj['active']:
+        continue
+    print("Working on {}".format(proj['name']))
+    path = root.format(proj['path'])
+    config['PROJECT_PATH'] = path
+    config['BUILD_COMMAND'] = proj['build']
+    config['ANNOTATION']['NULLABLE'] = proj['annot']['nullable']
+    config['ANNOTATION']['INITIALIZER'] = proj['init']['init']
     with open("config.json", "w") as f:
-        json.dump(data, f)
+        json.dump(config, f)
 
-    start = time.time()
-    os.system("python3 run.py run")
-    duration = time.time() - start
+    for i in range(0, 1):
+        branch = "deep_{}".format(i)
+        COMMAND = "cd {} && {}".format(config['PROJECT_PATH'], "{}")
+        os.system(COMMAND.format("git reset --hard"))
+        os.system(COMMAND.format("git fetch"))
+        os.system(COMMAND.format("git pull"))
+        os.system(COMMAND.format("git checkout nullaway"))
+        os.system(COMMAND.format("git branch -D {}".format(branch)))
+        os.system(COMMAND.format("git push origin --delete {}".format(branch)))
+        os.system(COMMAND.format("git checkout -b {}".format(branch)))
+        config["DEPTH"] = i
+        with open("config.json", "w") as f:
+            json.dump(config, f)
 
-    with open("{}/elapsed_time.txt".format(data['PROJECT_PATH']), "w") as f:
-        f.write(str(duration))
+        start = time.time()
+        os.system("python3 run.py run")
+        duration = time.time() - start
 
-    os.system("cd /tmp && zip -r annotator.zip NullAwayFix/")
-    os.system("mv /tmp/annotator.zip {}/annotator.zip".format(data['PROJECT_PATH']))
+        with open("{}/elapsed_time.txt".format(config['PROJECT_PATH']), "w") as f:
+            f.write(str(duration))
 
-    os.system(COMMAND.format("git add ."))
-    os.system(COMMAND.format("git commit -m \"Final Result\""))
-    os.system(COMMAND.format("git push --set-upstream origin {}".format(branch)))
-    exit()
+        os.system("cd /tmp && zip -r annotator.zip NullAwayFix/")
+        os.system("mv /tmp/annotator.zip {}/annotator.zip".format(config['PROJECT_PATH']))
+
+        os.system(COMMAND.format("git add ."))
+        os.system(COMMAND.format("git commit -m \"Final Result\""))
+        os.system(COMMAND.format("git push --set-upstream origin {}".format(branch)))
